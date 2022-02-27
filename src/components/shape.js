16db2a4643185e6da9b53006f2dea11c66d71361
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-02-27 14:28:28 +0530

    string

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
+class Shape {
+  constructor(color) {
+    this.color = "blue";
+  }
+  calculateArea() {}
+  drawShape() {}
+}
 
+module.exports = Shape;
 // export class using module.exports
